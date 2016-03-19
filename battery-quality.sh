820b357ddd81ec88d3cc45431e7c40f3bf9a174e
Armin <armin@m2m.pm>
2016-03-19 12:34:00 +0100

    add files

---
+++ battery-quality.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo -n "Your laptop battery has "
+
+p="$(find /sys/devices -name "*BAT0")"; full="$(cat "$p"/energy_full)"; design="$(cat "$p"/energy_full_design)"; l="$(echo "$full / $design * 100" | bc -q -l)"; echo -n "$l"
+
+echo -n "% of its factory design capacity level. "
+echo "Have a nice day."
+
+
+
