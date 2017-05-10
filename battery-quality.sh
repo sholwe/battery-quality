ce48102a34de6d54e7e48975b0a6dcc7e95a63b7
Just another S. Hol <s.s.holwegner@gmail.com>
2017-05-10 11:17:14 -0700

    Fixed for Linux 4.11 - s/energy/charge/
    Updated to Linux 4.11, and tested this little tool.  ../energy(.*) are now ../charge(.*) on four test systems (HP, Dell, Gateway, Toshiba) with Linux 4.11.0 under Void.  Standard "Works for me" clause applies.

--- battery-quality.sh
+++ battery-quality.sh
@@ -2,7 +2,7 @@
 
 echo -n "Your laptop battery has "
 
-p="$(find /sys/devices -name "*BAT0")"; full="$(cat "$p"/energy_full)"; design="$(cat "$p"/energy_full_design)"; l="$(echo "$full / $design * 100" | bc -q -l | cut -b -5)"; echo -n "$l"
+p="$(find /sys/devices -name "*BAT0")"; full="$(cat "$p"/charge_full)"; design="$(cat "$p"/charge_full_design)"; l="$(echo "$full / $design * 100" | bc -q -l | cut -b -5)"; echo -n "$l"
 
 echo -n "% of its factory design capacity level. "
 echo "Have a nice day."
